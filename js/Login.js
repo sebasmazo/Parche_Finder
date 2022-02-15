7be51242da8fbb12b7b52c9b542633e2a28c2c47
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-02-15 09:43:05 -0500

    Form Login 0.8
    Falta verificación de campos

--- js/Login.js
+++ js/Login.js
@@ -1,6 +1,15 @@
 function Login() {
-    
+    var credenciales = document.getElementsByName("credenciales")
+    console.log(credenciales)
+    alert("Bienvenido, " + credenciales.item(0).value)
+
 }
-function Register() {
-    
+function pulsar(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();
+        Login();
+    }
+}
+function test(){
+    print(credenciales)
 }
