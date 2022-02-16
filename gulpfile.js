5b8e09fd02395500ab0219be6d2f01fd717c4e6f
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-02-16 15:23:00 -0500

    MinCss
    CSS Minimizado con modulo gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const {src,dest} = require('gulp');
 const rename = require('gulp-rename');
 const sass =  require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean');
+const cleanCSS = require('gulp-clean-css');
 function css() {
   // place code for your default task here
   return src("./sass/**/*.scss")
