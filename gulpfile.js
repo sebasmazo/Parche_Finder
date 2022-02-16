cb0d3e94fc151a589aff3d4aaf49829b1c346b0b
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-02-16 15:16:46 -0500

    CleanCSS
    Gulp-Clean implementado

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const {src,dest} = require('gulp')
+const {src,dest} = require('gulp');
+const rename = require('gulp-rename');
 const sass =  require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean');
 function css() {
   // place code for your default task here
   return src("./sass/**/*.scss")
   .pipe(sass())
+  .pipe(cleanCSS())
+  .pipe(rename('main.min.css'))
   .pipe(dest("./css"))
 }
 exports.css = css;
