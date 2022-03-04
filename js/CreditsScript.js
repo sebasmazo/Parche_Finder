9c073660443010b0f759877498f38190950d6cbe
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-03-04 09:14:40 -0500

    Commit reversible

--- js/CreditsScript.js
+++
@@ -1,8 +0,0 @@
-var regresar = document.querySelector(".regresar");
-
-var regresarrec= new Hammer(regresar);
-regresarrec.on("panleft panright tap press", function (ev) {
-	if (ev.type == "tap") {
-		window.location.href="./Settings.html"
-	}
-});
